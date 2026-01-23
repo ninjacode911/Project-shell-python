67771fc4c4381a3294ef2ad2ef6d36de2dbbcb2f
Navnit <navnitamrutharaj1234@gmail.com>
2026-01-23 18:06:22 +0530

    adding user s command

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ") # this lets you write '$' to the console
-    pass
 
 # Captures the user's command in the "command" variable
 command=input()
