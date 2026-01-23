5fd7450621031eff25c2894e8b01cdb434865c94
Navnit <navnitamrutharaj1234@gmail.com>
2026-01-23 22:35:51 +0530

    add continious loop

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import sys
 def main():
-    sys.stdout.write("$ ") # this lets you write '$' to the console
+    while True: # this adds an infinite loop to make sure the program runs continuously
+        sys.stdout.write("$ ") # this lets you write '$' to the console
 
-    command = input() # Captures the user's command in the "command" variable
-    print(f"{command}: command not found") # prints the command not found message
+        command = input() # Captures the user's command in the "command" variable
+        print(f"{command}: command not found") # prints the command not found message
+
 if __name__ == "__main__":
     main()
