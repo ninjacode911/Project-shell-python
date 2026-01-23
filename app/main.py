33b7bf7cbe1694b06fe442a11c9d387caaa753b5
Navnit <navnitamrutharaj1234@gmail.com>
2026-01-23 22:44:20 +0530

    adding exit function

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@ import sys
 def main():
     while True: # this adds an infinite loop to make sure the program runs continuously
         sys.stdout.write("$ ") # this lets you write '$' to the console
-
         command = input() # Captures the user's command in the "command" variable
-        print(f"{command}: command not found") # prints the command not found message
+
+        if command == "exit":
+            break
+        else:
+            print(f"{command}: command not found") # prints the command not found message
+
 
 if __name__ == "__main__":
     main()
