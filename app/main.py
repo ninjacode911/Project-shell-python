67ab7a70fcb3c1058f7cbad1be8d0f53e1db1a11
Navnit <navnitamrutharaj1234@gmail.com>
2026-01-23 18:09:25 +0530

    test

--- app/main.py
+++ app/main.py
@@ -1,14 +1,8 @@
 import sys
-
-
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ") # this lets you write '$' to the console
 
-# Captures the user's command in the "command" variable
-command=input()
-print(f'{command}: command not found')
-
-
+    command = input() # Captures the user's command in the "command" variable
+    print(f"{command}: command not found") # prints the command not found message
 if __name__ == "__main__":
     main()
