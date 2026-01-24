89237303959a7e740a42dce144e4eec05a9cc8ab
Navnit <navnitamrutharaj1234@gmail.com>
2026-01-24 15:16:01 +0530

    adding path functionality

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 import sys
+import os
+
 def main():
     while True: # this adds an infinite loop to make sure the program runs continuously
         sys.stdout.write("$ ") # this lets you write '$' to the console
         command = input() # Captures the user's command in the "command" variable
         parts = command.split() # splits the command into a list of words
+
+
 
 
         if command == "exit":
@@ -16,7 +20,17 @@ def main():
             if cmd_to_check in ['echo', 'exit', 'type']:
                 print(f"{cmd_to_check} is a shell builtin")
             else:
-                print(f"{cmd_to_check} not found")
+                #search in path
+                path_env = os.environ('PATH','')
+                directories = path_env.split(os.pathsep)
+                found = False
+                for directory in directories:
+                    if os.path.exists(os.path.join(directory, cmd_to_check)):
+                        print(f"{cmd_to_check} is in {directory}")
+                        found = True
+                        break
+                if not found:
+                    print(f"{cmd_to_check} not found")
         else:
             print(f"{command}: command not found") # prints the command not found message
 
