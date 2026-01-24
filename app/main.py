26941e3abf8f9922014cb96104f30486c8c807ca
Navnit <navnitamrutharaj1234@gmail.com>
2026-01-24 15:32:09 +0530

    creating execution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-import sys
-import os
-
+import sys #importing sys module to interact with the system
+import os   #importing os module to interact with the operating system
+import subprocess #importing subprocess module to run external commands
 def main():
     while True: # this adds an infinite loop to make sure the program runs continuously
         sys.stdout.write("$ ") # this lets you write '$' to the console
@@ -33,7 +33,24 @@ def main():
                 if not found:
                     print(f"{cmd_to_check}: not found")
         else:
-            print(f"{command}: command not found") # prints the command not found message
+            #not a bulletin but try to run as an external program
+            #split to get program name and arguments
+            if len(parts) == 0:
+                continue
+            
+            program_name = parts[0]
+
+            path_env = os.environ.get('PATH','')
+            directories = path_env.split(os.pathsep)
+
+            found= False
+            for directory in directories:
+                file_path = os.path.join(directory, program_name)
+                if os.path.exists(file_path) and os.access(file_path, os.X_OK):
+                    found = True
+                    break
+            if found:
+                print(f"{command}: command not found") # prints the command not found message
 
 
 if __name__ == "__main__":
