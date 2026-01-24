01b07b09614e2b5b46401680e4afc4247372c4bd
Navnit <navnitamrutharaj1234@gmail.com>
2026-01-24 14:50:39 +0530

    Adding Echo

--- app/main.py
+++ app/main.py
@@ -3,9 +3,13 @@ def main():
     while True: # this adds an infinite loop to make sure the program runs continuously
         sys.stdout.write("$ ") # this lets you write '$' to the console
         command = input() # Captures the user's command in the "command" variable
+        parts = command.split() # splits the command into a list of words
+
 
         if command == "exit":
             break
+        elif command.startswith("echo"):
+            print(" ".join(parts[1:])) # joins the list of words back into a string
         else:
             print(f"{command}: command not found") # prints the command not found message
 
